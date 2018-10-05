f6d59d5f22b2fc636c96b058ab6ade379f365356
Colin Rupp <colin@thinkful.com>
2018-10-05 12:11:32 -0700

    did stuff

--- react-hot-cold/src/components/top-nav.js
+++ react-hot-cold/src/components/top-nav.js
@@ -6,42 +6,63 @@ import './top-nav.css';
 //redux
 import { connect  } from 'react-redux';
 
+export function TopNav(props) {
+  const { guesses, feedback } = props;
+  // If there's not exactly 1 guess, we want to
+  // pluralize the nouns in this aural update.
+  const pluralize = guesses.length !== 1;
+  let auralStatus = `Here's the status of the game right now: ${feedback} You've made ${
+    guesses.length
+  } ${pluralize ? 'guesses' : 'guess'}.`;
+  if (guesses.length > 0) {
+    auralStatus += ` ${
+      pluralize ? 'In order of most- to least-recent, they are' : 'It was'
+    }: ${guesses.reverse().join(', ')}`;
+  }
 
-export default function TopNav(props) {
-  return (
-    <nav>
-      <ul className="clearfix">
-        <li>
-          <a
-            href="#what"
-            className="what"
-            aria-label="How to play"
-          >
-            What?
-          </a>
-        </li>
-        <li>
-          <a
-            href="#feedback"
-            className="new"
-            aria-label="Start a new game"
-            onClick={() => store.dispatch(restartGame())}
-          >
-            + New Game
-          </a>
-        </li>
-        <li>
-          <a
-            href="#get-status"
-            /* the `visuallyhidden` class hides an element
-            while leaving it available to screen reader users  */
-            className="visuallyhidden focusable status-link"
-            onClick={() => store.dispatch(auralStatus())}
-          >
-            Hear state of game
-          </a>
-        </li>
-      </ul>
-    </nav>
-  );
-}
+    return (
+      <nav>
+        <ul className="clearfix">
+          <li>
+            <a
+              href="#what"
+              className="what"
+              aria-label="How to play"
+            >
+              What?
+            </a>
+          </li>
+          <li>
+            <a
+              href="#feedback"
+              className="new"
+              aria-label="Start a new game"
+              onClick={() => store.dispatch(restartGame())}
+            >
+              + New Game
+            </a>
+          </li>
+          <li>
+            <a
+              href="#get-status"
+              /* the `visuallyhidden` class hides an element
+              while leaving it available to screen reader users  */
+              className="visuallyhidden focusable status-link"
+              onClick={(auralStatus) => store.dispatch(auralStatus(auralStatus))}
+            >
+              Hear state of game
+            </a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+
+
+  const mapStateToProps = state => ({
+    guesses: state.guesses,
+    feedback: state.feedback,
+    auralStatus: state.auralStatus
+  });
+
+  export default connect(mapStateToProps)(TopNav);
--- react-hot-cold/src/reducers/reducers.js
+++ react-hot-cold/src/reducers/reducers.js
@@ -32,27 +32,6 @@ export const gameReducer = (state = initialState, action) => {
     });
     // Action type is aural status
   } else if (action.type === AURAL_STATUS) {
-      const { guesses, feedback } = this.state;
-
-    // If there's not exactly 1 guess, we want to
-    // pluralize the nouns in this aural update.
-    const pluralize = guesses.length !== 1;
-
-    let auralStatus = `Here's the status of the game right now: ${feedback} You've made ${
-      guesses.length
-    } ${pluralize ? 'guesses' : 'guess'}.`;
-
-    if (guesses.length > 0) {
-      auralStatus += ` ${
-        pluralize ? 'In order of most- to least-recent, they are' : 'It was'
-      }: ${guesses.reverse().join(', ')}`;
-    }
-
-    this.setState({ auralStatus });
-  }
-
-
-
     return Object.assign({}, state, {
       auralStatus: action.auralStatus
     });
